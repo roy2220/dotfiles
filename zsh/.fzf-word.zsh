ac504752e2c034d7da0309e15bc87fdff4b30d66
Roy O'Young <roy2220@outlook.com>
2021-04-05 19:45:27 +0800

    Improve .fzf-word.zsh

--- zsh/.fzf-word.zsh
+++ zsh/.fzf-word.zsh
@@ -1,27 +1,34 @@
 fzf-word() {
     local words
     words=$(tmux capture-pane -p | python2 -c '\
+import itertools
 import re
 import sys
 
 screen = sys.stdin.read()
 words1 = set(screen.split("\n"))
+
+pattern = re.compile(r"\s+")
 words2 = set()
 for word in words1:
-    words2.update(re.split(r"\s+", screen))
+    words2.update(pattern.split(word))
+
+delimiters = (r"\-", r"\.", r"@", r":", r"/")
 words3 = set()
-for word in words2:
-    words3.update(re.findall(r"[a-zA-Z0-9_\-\.]+", word))
+for n in range(0, len(delimiters) + 1):
+    for sub_delimiters in itertools.combinations(delimiters, n):
+        pattern = re.compile(r"[_a-zA-Z0-9{}]+".format("".join(sub_delimiters)))
+        for word in words2:
+            words3.update(pattern.findall(word))
+
+pattern = re.compile(r"[a-zA-Z]+|[0-9]+")
 words4 = set()
 for word in words3:
-    words4.update(re.findall(r"[a-zA-Z0-9_]+", word))
-words5 = set()
-for word in words4:
-    words5.update(re.findall(r"[a-zA-Z]+", word))
-words6 = set()
-for word in words4:
-    words6.update(re.findall(r"[0-9]+", word))
-for word in sorted(words1.union(words2).union(words3).union(words4).union(words5).union(words6)):
+    words4.update(pattern.findall(word))
+
+words = words1.union(words2).union(words3).union(words4)
+words.remove("")
+for word in sorted(words):
     print(word)
 ')
     local word
