11eaf4f170d62537a5000849772198eace8f7daf
Roy O'Young <roy2220@outlook.com>
2024-03-26 20:03:37 +0800

    Add ChatGPT

---
+++ local/.local/src/1-install-chatgpt.bash
@@ -0,0 +1,17 @@
+set -eu${DEBUG+x}o pipefail
+
+case $(arch) in
+x86_64)
+	ARCH=amd64
+	;;
+aarch64)
+	ARCH=arm64
+	;;
+*)
+	echo 'unknown architecture' 1>&2
+	exit 1
+	;;
+esac
+
+DOWNLOAD_URL=https://github.com/kardolus/chatgpt-cli/releases/latest/download/chatgpt-linux-${ARCH}
+curl -SsLf "${DOWNLOAD_URL}" | install /dev/stdin "${HOME}/.local/bin/chatgpt"
