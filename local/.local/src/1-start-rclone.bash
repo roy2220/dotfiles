bde522d4ccf3979122664b13bc1be2c718e37bf5
Roy O'Young <roy2220@outlook.com>
2025-03-04 02:10:14 +0800

    Update 1-start-rclone.bash

--- local/.local/src/1-start-rclone.bash
+++ local/.local/src/1-start-rclone.bash
@@ -2,6 +2,10 @@ set -eu${DEBUG+x}o pipefail
 
 mkdir --parents /gdrive
 rclone mount --daemon gdrive: /gdrive
+# mkdir --parents /gdrive.shared-with-me
+# rclone mount --daemon --drive-shared-with-me gdrive: /gdrive.shared-with-me
+# mkdir --parents /gdrive.trashed
+# rclone mount --daemon --drive-trashed-only gdrive: /gdrive.trashed
 
 mkdir --parents /oss
 rclone mount --daemon oss:/oyy1993 /oss
